91eea4902025eb2d498598bccb46ad6a2492c2f9
Alif Nursetyo Vimanto <alifvimanto69@gmail.com>
2025-05-08 20:22:24 +0700

    Commit ke 1

---
+++ Tiket/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiket
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Selamat datang di Ferizy");
+            Console.WriteLine("Silakan masukkan data anda untuk pemesanan tiket:");
+
+            Tiket tiket = new Tiket();
+
+            Console.Write("Nama: ");
+            tiket.Nama = Console.ReadLine();
+
+            Console.Write("NIK: ");
+            tiket.NIK = Console.ReadLine();
+
+            Console.Write("Nomor Telepon: ");
+            tiket.NomorTelepon = Console.ReadLine();
+
+            Console.Write("Nomor Kartu Vaksin: ");
+            tiket.NomorKartuVaksin = Console.ReadLine();
+
+            Console.Write("Alamat Rumah: ");
+            tiket.AlamatRumah = Console.ReadLine();
+
+            Console.WriteLine();
+            tiket.PrintDetails();
+
+            Console.WriteLine("Terima kasih telah melakukan pemesanan tiket. Selamat menikmati perjalanan!");
+        }
+    }
+}
---
+++ Tiket/Tiket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiket
+{
+    public class Tiket
+    {
+        public string Nama { get; set; }
+        public string NIK { get; set; }
+        public string NomorTelepon { get; set; }
+        public string NomorKartuVaksin { get; set; }
+        public string AlamatRumah { get; set; }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("Detail Pemesanan Tiket:");
+            Console.WriteLine($"Nama: {Nama}");
+            Console.WriteLine($"NIK: {NIK}");
+            Console.WriteLine($"Nomor Telepon: {NomorTelepon}");
+            Console.WriteLine($"Nomor Kartu Vaksin: {NomorKartuVaksin}");
+            Console.WriteLine($"Alamat Rumah: {AlamatRumah}");
+            Console.WriteLine("Rute Perjalanan: Ketapang - Gilimanuk");
+            Console.WriteLine("Jenis Transportasi: Kapal Laut");
+        }
+    }
+}
